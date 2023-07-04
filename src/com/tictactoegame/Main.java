f5084b83c2687a9787c2ab42a0651f5da68fcba2
Amit <amitlpalandurkar1998@gmail.com>
2023-07-04 15:28:10 +0530

    Tic tac toe Game. file Structure.

---
+++ src/com/tictactoegame/Main.java
@@ -0,0 +1,9 @@
+package com.tictactoegame;
+
+public class Main {
+    public static void main(String[] args) {
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+
+        ticTacToeGame.tictacBoard();
+    }
+}
---
+++ src/com/tictactoegame/TicTacToeGame.java
@@ -0,0 +1,22 @@
+package com.tictactoegame;
+//As a Player would like to
+//start fresh by creating a tic
+//tac toe board - Create a TicTacToeGame class - Create method for every use case
+//and call from main
+//- Create a board of char[] of size 10
+//and assign empty space to each
+//element
+//
+//-
+//0th index is ignored to make it
+//
+//UC 1 user friendly
+public class TicTacToeGame {
+
+    public void tictacBoard(){
+        char [] tictac = new char[10];
+        tictac[0] = ' ';
+    }
+
+
+}
